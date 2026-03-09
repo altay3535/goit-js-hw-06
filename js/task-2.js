fe6fed816a8eba21eb80e2bd12ddad4ea1eb7e0f
Sevcan Ozdemir <colaksevcan@gmail.com>
2026-03-09 21:33:21 +0300

    odev gonderildi

---
+++ js/task-2.js
@@ -0,0 +1,26 @@
+class storage {
+    #items;
+    constructor(items) {
+        this.#items = items;
+    }
+    getItems() {
+        return this.#items;
+    }
+    addItem(newItem) {
+        this.#items.push(newitem);
+    }
+    removeItem(itemToRemove) {
+        this.items = this.#items.filter(
+            item => item !== itemToRemove
+        );
+    }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
